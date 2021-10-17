3aabab66fbc150d8694849e52b450046aae2395e
Mateusz <mat.bereda@gmail.com>
2021-10-17 15:45:24 +0200

    komentarz

---
+++ src/pl/camp/it/nawiasy/App.java
@@ -0,0 +1,26 @@
+package pl.camp.it.nawiasy;
+
+public class App {
+    public static void main(String[] args) {
+        String s = "Al(a ma )(kot)a)(";
+
+        char[] cars = s.toCharArray();
+
+        int licznik = 0;
+        for (char c : cars) {
+            if(c == '(') {
+                licznik++;
+            } else if(c == ')') {
+                licznik--;
+            }
+            if(licznik < 0) {
+                System.out.println("Nawiasy nieprawidłowe");
+                return;
+            }
+        }
+
+        if(licznik == 0) {
+            System.out.println("Nawiasy się zgadzają !!");
+        }
+    }
+}
